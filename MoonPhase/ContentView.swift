b8c1466b2f2af931aa187d46ebbaec8ae7ed00e1
Lorand Ignat <lorand.ignat@nagarro.com>
2023-12-07 12:31:46 +0200

    Added animation to the toggle

--- MoonPhase/ContentView.swift
+++ MoonPhase/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
   private let scene = SolarSystemPlanetScene()
   private let backgroundScene = SolarSystemCameraScene()
   
-  @State private var viewType = 0
+  @State private var viewType: UInt = 0
   @State private var animateGradient = false
   @State private var animateViewChange = false
   
@@ -25,40 +25,39 @@ struct ContentView: View {
       Color.black
         .ignoresSafeArea()
       
-        PlanetSceneView(scene: backgroundScene)
-          .opacity(animateViewChange ? 1 : 0.5)
-          .ignoresSafeArea()
-        
-        VStack {
-          if animateViewChange {
-            Spacer()
-          }
-          LinearGradient(colors: [animateViewChange ? .clear : .clear, .gradient3, .gradient2, .gradient1],
-                         startPoint: animateGradient ?  UnitPoint(x: 0.0, y: 0.1) : UnitPoint(x: 0.1, y: 0.1),
-                         endPoint: animateGradient ? UnitPoint(x: 0, y: 0.3) : UnitPoint(x: 0, y: 0.7))
-          .onAppear {
-            withAnimation(.easeInOut(duration: 10.0).repeatForever(autoreverses: true)) {
-              animateGradient.toggle()
-            }
-          }
-          .frame(maxHeight: animateViewChange ? 200 : .infinity)
-        }
+      PlanetSceneView(scene: backgroundScene)
+        .opacity(animateViewChange ? 1 : 0.5)
         .ignoresSafeArea()
-        
-        
-        PlanetSceneView(scene: scene)
-          .ignoresSafeArea()
-        
+      
+      VStack {
+        if animateViewChange {
+          Spacer()
+        }
+        LinearGradient(colors: [animateViewChange ? .clear : .clear, .gradient3, .gradient2, .gradient1],
+                       startPoint: animateGradient ?  UnitPoint(x: 0.0, y: 0.1) : UnitPoint(x: 0.1, y: 0.1),
+                       endPoint: animateGradient ? UnitPoint(x: 0, y: 0.3) : UnitPoint(x: 0, y: 0.7))
+        .onAppear {
+          withAnimation(.easeInOut(duration: 10.0).repeatForever(autoreverses: true)) {
+            animateGradient.toggle()
+          }
+        }
+        .frame(maxHeight: animateViewChange ? 200 : .infinity)
+      }
+      .ignoresSafeArea()
+      
+      PlanetSceneView(scene: scene)
+        .ignoresSafeArea()
+      
       GeometryReader { geometry in
         VStack {
           Spacer().frame(alignment: .bottom)
           ZStack {
-              Color(.gradient1)
-                .background(.ultraThinMaterial)
-                .cornerRadius(15)
-                .frame(maxHeight: animateViewChange ? 200 : geometry.size.height / 5 * 2, alignment: .bottom)
-                .shadow(radius: 5)
-                .opacity(animateViewChange ? 0.7 : 1)
+            Color(.gradient1)
+              .background(.ultraThinMaterial)
+              .cornerRadius(15)
+              .frame(maxHeight: animateViewChange ? 200 : geometry.size.height / 5 * 2, alignment: .bottom)
+              .shadow(radius: 5)
+              .opacity(animateViewChange ? 0.7 : 1)
             detailView
               .colorMultiply(.teal)
               .saturation(0.3)
@@ -79,62 +78,66 @@ struct ContentView: View {
   }
   
   var detailView: some View {
-      VStack {
-        Spacer().frame(alignment: .bottom)
-        let layout = !animateViewChange ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
-        layout {
-          Text(scene.displayingDate(), style: .date)
-            .font(animateViewChange ? .callout : .title2)
-            .foregroundStyle(.white)
-          Text(scene.displayingDate(), style: .time)
-            .font(animateViewChange ? .callout : .title2)
-            .foregroundStyle(.white)
-        }
-        
-        if !animateViewChange {
-          Spacer().frame(alignment: .bottom)
-        }
-        VStack {
-          if !animateViewChange {
-            HStack {
-              Text("Moon phase:")
-                .foregroundStyle(.white)
-              Text("\(scene.moonPhase())")
-                .animation(.none)
-                .foregroundStyle(.white)
-            }
-            Spacer().frame(alignment: .bottom)
-            HStack {
-              Text("Illumination percentage:")
-                .foregroundStyle(.white)
-              Text("\(scene.moonIllumination(), specifier: "%.2f") %")
-                .animation(.none)
-                .foregroundStyle(.white)
-            }
-          }
-        }
-        if !animateViewChange {
-          Spacer().frame(alignment: .bottom)
-        }
-        VStack {
-          Slider(value: $days, in: -29.53059...29.53059)
-            .padding(EdgeInsets(top: 10, leading: 70, bottom: 10, trailing: 70))
-            .tint(.gray.opacity(0.4))
-            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-
-          ViewSelectorSlider { valueSelected in
-            viewType = valueSelected
-            withAnimation {
-              animateViewChange = valueSelected != 0
-            }
-          }
-          .frame(width: 270)
-        }
-        .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+    VStack {
+      Spacer().frame(alignment: .bottom)
+      let layout = !animateViewChange ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+      layout {
+        Text(scene.displayingDate(), style: .date)
+          .font(animateViewChange ? .callout : .title2)
+          .foregroundStyle(.white)
+        Text(scene.displayingDate(), style: .time)
+          .font(animateViewChange ? .callout : .title2)
+          .foregroundStyle(.white)
       }
+      
+      if !animateViewChange {
+        Spacer().frame(alignment: .bottom)
+      }
+      VStack {
+        if !animateViewChange {
+          HStack {
+            Text("Moon phase:")
+              .foregroundStyle(.white)
+            Text("\(scene.moonPhase())")
+              .animation(.none)
+              .foregroundStyle(.white)
+          }
+          Spacer().frame(alignment: .bottom)
+          HStack {
+            Text("Illumination percentage:")
+              .foregroundStyle(.white)
+            Text("\(scene.moonIllumination(), specifier: "%.2f") %")
+              .animation(.none)
+              .foregroundStyle(.white)
+          }
+        }
+      }
+      if !animateViewChange {
+        Spacer().frame(alignment: .bottom)
+      }
+      VStack {
+        Slider(value: $days, in: -29.53059...29.53059)
+          .padding(EdgeInsets(top: 10, leading: 70, bottom: 10, trailing: 70))
+          .tint(.gray.opacity(0.4))
+          .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+        
+        SlingshotMultipleValueToggle(selectedValue: $viewType,
+                        icons: [
+                          Image(systemName: "bubble.circle"),
+                          Image(systemName: "moon.circle"),
+                          Image(systemName: "sun.horizon.circle")])
+        .frame(width: 270, height: 40)
+        .onChange(of: viewType) {
+          withAnimation {
+            animateViewChange = viewType != 0
+          }
+        }
+      }
+      .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
+    }
   }
   
-  func moveCamera(for viewType: Int, animated: Bool) {
+  func moveCamera(for viewType: UInt, animated: Bool) {
     if viewType == 0 {
       backgroundScene.moveCameraToMoonView(animated: animated, centered: false)
       scene.moveCameraToMoonView(animated: animated, centered: false)
